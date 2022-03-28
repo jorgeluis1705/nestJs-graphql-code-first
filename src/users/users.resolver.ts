04d7d649163550f9dada92329306c0aa6eff7ee9
jorge Luis Pena Real <falmemor@gmail.com>
2022-03-27 21:12:38 -0600

    final

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,17 +1,24 @@
+import { Observable } from 'rxjs';
 import { UsersService } from './users.service';
 import { UserInput } from './dto/user.input';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User as UserGraphql } from './models/user.model';
-import { User } from './models/user.document.schema';
+import { User as UserMongo } from './models/user.document.schema';
 @Resolver((of) => UserGraphql)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
   @Query((returns) => [UserGraphql], { nullable: 'itemsAndList' })
-  getAll(): any {
-    return [];
+  getAll(): Observable<UserMongo[]> {
+    return new Observable((obs) => {
+      this.usersService.findAll().subscribe({
+        next: (resp) => obs.next(resp),
+        error: (err) => obs.error(err),
+        complete: () => obs.complete(),
+      });
+    });
   }
   @Mutation((returns) => UserGraphql)
-  async createItem(@Args('input') input: UserInput): Promise<User> {
+  async createItem(@Args('input') input: UserInput): Promise<UserMongo> {
     return await this.usersService.create(input);
   }
 }

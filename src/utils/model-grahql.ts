5d3296e02ee93f1b9816d2a7ce3a1b5a66c61788
jorge Luis Pena Real <falmemor@gmail.com>
2022-03-27 21:31:49 -0600

    types

--- src/utils/model-grahql.ts
+++ src/utils/model-grahql.ts
@@ -1,8 +1,9 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { DynamicModule } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { DirectiveLocation, GraphQLDirective } from 'graphql';
 
-export const graphqlModuleConfiguration =
+export const graphqlModuleConfiguration: DynamicModule =
   GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: 'src/schema.gql',
--- src/utils/mongoDbConnection.ts
+++ src/utils/mongoDbConnection.ts
@@ -1,5 +1,6 @@
+import { DynamicModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-export const mongoDbConnection = MongooseModule.forRoot(
+export const mongoDbConnection: DynamicModule = MongooseModule.forRoot(
   'mongodb+srv://node_user:pBYi9t$bOcal2^C@cluster0.ayyll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 );

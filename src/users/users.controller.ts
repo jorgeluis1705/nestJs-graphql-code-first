e360e422b1d63f33d02505beb17d59b85b9d692b
jorge Luis Pena Real <falmemor@gmail.com>
2022-03-27 21:26:31 -0600

    final

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UsersService } from './users.service';
+import { User as UserMongo } from './models/user.document.schema';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('/')
+  getAll(): Observable<UserMongo[]> {
+    return new Observable((obs) => {
+      this.usersService.findAll().subscribe({
+        next: (resp) => obs.next(resp),
+        error: (err) => obs.error(err),
+        complete: () => obs.complete(),
+      });
+    });
+  }
+}
